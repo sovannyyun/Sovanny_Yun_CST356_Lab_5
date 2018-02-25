e0cbf358e55a463787ae168ee4570dd4b787cfac
sovannyyun <sovanny.yun.cis@gmail.com>
2018-02-24 19:59:44 -0800

    InitialCommit

---
+++ Sovanny_Yun_CST356_Lab5/scripts/app.js
@@ -0,0 +1,9 @@
+﻿function submitRegistration() {
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+    var age = document.getElementById("age").value;
+
+    var summary = firstName + " " + lastName + " is " + age + " years old.";
+
+    document.getElementById("showResult").innerText = summary;
+}
